225e4d2c0dff4cfd8177e4a4785ac736cc9c5e44
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2020-11-29 15:49:55 -0800

    adds param checking and db retry

--- week1-function/HttpTrigger1/__init__.py
+++ week1-function/HttpTrigger1/__init__.py
@@ -2,6 +2,7 @@ import logging
 import pyodbc
 import json
 import os
+import time
 import azure.functions as func
 
 # Driver={ODBC Driver 13 for SQL Server};Server=tcp:serverless-fun.database.windows.net,1433;Database=serverless1;Uid=sqladmin;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
@@ -11,22 +12,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
     sqlConnectionString = os.environ["serverlessdb"]
+    turkeySize = 0
+    messages = []
+    statusCode = 200
+    ingredients = []
+
+    try:
+        turkeySize = float(req.params.get('turkey'))
+        logging.info(str(turkeySize))
+    except:
+        messages.append('Use the query string "turkey" to send a turkey weight.')
+        return generateHttpResponse(ingredients, messages, 400)
+
+    try:
+        ingredients = getIngredients(sqlConnectionString, turkeySize)
+    except pyodbc.DatabaseError:
+        time.sleep(45)
+        ingredients = getIngredients(sqlConnectionString, turkeySize)
+        messages.append('Thanks for waithing, the database had to be started.')
+
+    return generateHttpResponse(ingredients, messages, statusCode)
+
+def generateHttpResponse(ingredients, messages, statusCode):
+    return func.HttpResponse(
+        json.dumps({"Messages": messages, "Ingredients": ingredients}, sort_keys=True, indent=4),
+        status_code=statusCode
+    )
+
+
+def getIngredients(sqlConnectionString, turkeySize):
+    logging.info('contacting DB')
+    results = []
     sqlConnection = pyodbc.connect(sqlConnectionString)
     sqlCursor = sqlConnection.cursor()
-
-    turkeySize = req.params.get('turkey')
-
-    sqlCursor.execute('EXEC calculateRecipe '+turkeySize)
-
+    sqlCursor.execute('EXEC calculateRecipe '+str(turkeySize))
     columns = [column[0] for column in sqlCursor.description]
-
-    results = []
     for row in sqlCursor.fetchall():
         results.append(dict(zip(columns, row)))
-
-    output = {"Ingredients": results}
-
-    return func.HttpResponse(
-        json.dumps(output, sort_keys=True, indent=4),
-        status_code=200
-    )
+    return results
